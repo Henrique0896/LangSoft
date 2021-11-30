d086f74ecd3c09d3d10a790ebf90f776e12d55f9
Henrique0896 <barrosohenriquelima@gmail.com>
2021-11-30 19:51:12 -0300

    adicionando cleo

--- app/models/learningObjectModel.py
+++ app/models/learningObjectModel.py
@@ -101,15 +101,15 @@ class LearningObject(object):
             "palavra_chave": None
         }
         self.classificacao = {
-            "1": None,
-            "2": None,
-            "3": None,
-            "4": None,
-            "5": None,
-            "6": None,
-            "7": None,
-            "8": None,
-            "9": None,
+            "proposito": None,
+            "caminho_taxon": {
+                "fonte":None,
+                "taxon": None,
+                "id": None,
+                "entrada": None
+            },
+            "descricao": None,
+            "palavras_chave": None,
         }
     
 
